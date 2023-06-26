99e4e1b36199551f280167f55a42d6610c454645
Pileks <jure@amber-it.co>
2023-06-26 17:22:31 +0200

    Remove logo from wrap card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import DefaultWrapLogo from "@/components/default-wrap-logo";
-import Image from "next/image";
 import Link from "next/link";
 import { WrapHomepageItem, wraps } from "./wraps";
 import { encodeWrapUri } from "@/utils/wrapUri";
@@ -13,19 +11,10 @@ function WrapListItem(props: WrapListItemProps) {
   return (
     <Link
       href={`/wrap/${encodeWrapUri(wrap.uri)}/readme`}
-      className="flex gap-6 rounded-2xl border border-polywrap-iris-600 bg-polywrap-card p-8 hover:border-polywrap-iris-500 hover:bg-polywrap-card-hover"
+      className="flex flex-col gap-6 rounded-2xl border border-polywrap-iris-600 bg-polywrap-card p-8 hover:border-polywrap-iris-500 hover:bg-polywrap-card-hover"
     >
-      <div className="flex items-center justify-center">
-        {wrap.logoUrl && wrap.logoUrl.length ? (
-          <Image src={wrap.logoUrl} alt="Wrap logo" className="w-16" />
-        ) : (
-          <DefaultWrapLogo className="w-16" />
-        )}
-      </div>
-      <div className="flex flex-col gap-6">
-        <h3 className="text-xl font-bold text-polywrap-gray-50">{wrap.name}</h3>
-        <p className="text-polywrap-gray-200">{wrap.description}</p>
-      </div>
+      <h3 className="text-xl font-bold text-polywrap-gray-50">{wrap.name}</h3>
+      <p className="text-polywrap-gray-200">{wrap.description}</p>
     </Link>
   );
 }
--- src/components/default-wrap-logo.tsx
+++
@@ -1,5 +0,0 @@
-import WrapLogo from "@/assets/default-wrap-logo.svg";
-
-export default function DefaultWrapLogo({ className }: { className?: string }) {
-  return <WrapLogo className={className}></WrapLogo>;
-}
